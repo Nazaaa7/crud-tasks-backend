7f20ec0db0a21ca3139a415d9090937cfd8d4303
Nazarena Garcia <nazagarcia132@gmail.com>
2024-08-07 19:05:25 -0300

    Terminé el crud y elimine el .gitignore, porque estaba mal

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -1,102 +1,85 @@
-// const db = require('../bd/bd');
 
-// const ctrl = {};
-
-// // Controlador para crear un usuario
-// ctrl.obtenerTareas = (req, res) => {
-//     res.json(req.body)
-// }    
-
-// // Controlador para obtener todos los usuarios
-
-// // Controlador para obtener todas las tareas
-// ctrl.obtenerTareas = async (req, res) => {
-//   try {
-//     const [results] = await db.query('SELECT * FROM tasks');
-//     res.json(results);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).send('Database error.');
-//   }
-// };
-// // Controlador para obtener un usuario por su id
-
-// // Controlador para actualizar un usuario por su id
-
-// // Controlador para borrar un usuario por su id
-
-// module.exports = ctrl;
-
-
-// // app.get("/task/:id", async (req, res) =>{
-// //     const conexion =  await newConnection()
-// //     const id = req.params.id
-// //     const results = await conexion.query(`SELECT *  FROM tasks where id = ?`, id);
-
-// //     res.json(results[0]);
-
-// //     conexion.end()//siempre terminar la funcion 
-// // });
-
-
-
-// // app.delete("/task/:id", async (req, res) =>{
-// //     const conexion =  await newConnection()
-// //     const id = req.params.id
-// //     const results = await conexion.query(`DELETE  FROM tasks where id = ?`, id);
-
-// //     res.send("Tarea Eliminada")
-
-// //     conexion.end()//siempre terminar la funcion 
-// // });
-
-// // app.post("/task", async (req, res)=>{
-// //     console.log(req.body)
-// //     const conexion = await newConnection()
-// //     const { title } = req.body
-// //     const { descripcion } = req.body
-// //     const { isComplete } = req.body
-// //     conexion.query('INSERT INTO tasks (title, description, isComplete) values (?)', [title, descripcion, isComplete]);
-// //     res.send("crear");
-// //     conexion.end()//siempre terminar la funcion 
-    
-// // });
-
-
-// // app.put("/tasks/:id", async (req, res) => {
-// //     const conexion = await newConnection();
-
-// //     try {
-// //         const id = req.params.id;
-// //         const { descripcion } = req.body;
-
-// //         // Corrección en la llamada al método 'query' para usar el array de parámetros
-// //         const results = await conexion.query('UPDATE tabla1 SET descripcion = ? WHERE idTabla = ?', [descripcion, id]);
-
-// //         res.send("Tarea actualizada");
-// //     } catch (error) {
-// //         console.error(error);
-// //         res.status(500).send("Error actualizando la tarea");
-// //     } finally {
-// //        conexion.end(); // Asegurarse de cerrar la conexión en cualquier caso
-// //     }
-// // });
 const { connectDB } = require("../bd/bd");
 
 const ctrl = {};
-
+        
 // Controlador para obtener todas las tareas
 ctrl.obtenerTareas = async (req, res) => {
     try {
         const connection = await connectDB();
         const [results] = await connection.query('SELECT * FROM tasks');
+        
         res.json(results);
     } catch (error) {
         console.error(error);
-        res.status(500).send('Database error.');
+        res.status(500).send('Error con la base de datos');
+    }
+};
+// Controlador para obtener una tarea por su id
+ctrl.obtenerTarea = async (req, res) => {
+    try {
+        const connection = await connectDB();
+        const id = req.params.id
+        const [results] = await connection.query(`SELECT *  FROM tasks where id = ?`, id);
+        res.json(results[0]);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error con la base de datos');
+            }
+};
+
+// Controlador para agregar una tarea
+ctrl.agregarTarea = async (req, res) => {
+    try {
+        const connection = await connectDB();
+        const { title } = req.body
+        const { description } = req.body
+        const { isComplete } = req.body
+        
+        const [results] = await connection.query('INSERT INTO tasks (title, description, isComplete) values (?, ?, ?)', [title, description, isComplete]);
+        
+        res.send("Nueva tarea agregada");
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error con la base de datos');
     }
 };
 
+// Controlador para editar una tarea, por el id
+ctrl.editarTarea = async (req, res) => {
+    try {
+        const connection = await connectDB();
+        const id = req.params.id;
+        const { title } = req.body;
+        const { description } = req.body; 
+        const { isComplete } = req.body;
+        // Corrección en la llamada al método 'query' para usar el array de parámetros
+        const results = await connection.query('UPDATE tasks SET title = ? , description = ? , isComplete = ? WHERE id = ?', [title, description, isComplete, id]);
+        
+        res.send("Tarea editada");
+     } catch (error) {
+         console.error(error);
+         res.status(500).send("Error en la base de datos");
+         } finally {
+             if (connection) {
+                await connection.end(); // Asegurarse de cerrar la conexión 
+            }
+        }
+    };
 
-
-module.exports = ctrl;
+    // Controlador para eliminar una tarea
+ctrl.eliminarTarea = async (req, res) => {
+    try {
+        const connection = await connectDB();
+        const id = req.params.id;
+        const results = await connection.query(`DELETE  FROM tasks where id = ?`, id);
+    
+        res.send("Tarea eliminada");
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Error en la base de datos");
+        
+       }
+   };
+    module.exports = ctrl;
+    
--- src/routes/tareas.routes.js
+++ src/routes/tareas.routes.js
@@ -2,7 +2,15 @@ const express = require('express');
 const router = express.Router();
 const {
   obtenerTareas,
+  obtenerTarea,
+  agregarTarea,
+  editarTarea,
+  eliminarTarea
 } = require('../controllers/tasks.controller');
 
 router.get('/', obtenerTareas);
+router.get('/:id', obtenerTarea);
+router.post('/', agregarTarea);
+router.put('/:id', editarTarea);
+router.delete('/:id', eliminarTarea);
 module.exports = router;
